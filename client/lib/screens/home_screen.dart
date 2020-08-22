dc4c184efe69a77e46c2cddc1463991a2efcb5f6
MICHAL SOBIECKI <kontakt@codeshaker.pl>
2020-08-22 15:28:22 +0200

    Initialize shorty flutter client

---
+++ client/lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenPage extends StatefulWidget {
+  HomeScreenPage({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenPageState createState() => _HomeScreenPageState();
+}
+
+class _HomeScreenPageState extends State<HomeScreenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(),
+    );
+  }
+}
